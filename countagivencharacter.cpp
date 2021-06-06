eef153409e468ba0cad906d7d2048cdae109916c
Somu Sashank <77006885+Sashank-B@users.noreply.github.com>
2021-06-06 21:24:50 +0530

    Create countagivencharacter.cpp

---
+++ countagivencharacter.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+int count(string s, char c)
+{
+    int res=0;
+    for(int i=0;i<s.length();i++)
+     if(s[i]==c)
+     res++;
+    return res; 
+     
+}
+
+ int main(){
+     string str="geeksforgeeks";
+     char c='e';
+     cout<<count(str,c)<<endl;
+     return 0;
+ }
