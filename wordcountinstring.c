f3d618e3fa85a1cec4107b6e4403d07691dbd58f
Somu Sashank <77006885+Sashank-B@users.noreply.github.com>
2021-06-06 14:00:52 +0530

    Create wordcountinstring.c

---
+++ wordcountinstring.c
@@ -0,0 +1,19 @@
+#include <stdio.h>  
+#include <string.h>  
+   
+int main()  
+{  
+    char sentence[] = "Beauty lies in the eyes of beholder";  
+    int wordCount = 0;  
+      
+    for(int i = 0; i < strlen(sentence)-1; i++) {  
+      
+        if(sentence[i] == ' ' && isalpha(sentence[i+1]) && (i > 0)) {  
+            wordCount++;  
+        }  
+    }    
+    wordCount++;  
+    printf("Total number of words in the given string: %d", wordCount);  
+   
+    return 0;  
+}   
