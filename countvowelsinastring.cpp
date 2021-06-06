3bcac65c838f131871f37a70f08537c5f64f77ec
Somu Sashank <77006885+Sashank-B@users.noreply.github.com>
2021-06-06 21:30:27 +0530

    Create countvowelsinastring.cpp

---
+++ countvowelsinastring.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string>
+#include<vector>
+#include<iostream>
+using namespace std;
+bool isvowel(char ch)
+{
+    ch=toupper(ch);
+    return(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U');
+    
+}
+int countvowels(string str)
+{
+    int count=0;
+    for(int i=0;i<str.length();i++)
+     if(isvowel(str[i]))
+      ++count;
+    return count;
+}
+int main()
+{
+    string str="abc de iou";
+    cout<<countvowels(str)<<endl;
+    return 0;
+}
